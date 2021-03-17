5a9fd241f85b4a9278bec851c8c4867c2545147d
Zykov Maxim <maxmaxmax646@gmail.com>
2021-03-17 05:20:31 +0300

    Добавил просмотр содержимого коллекции.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
+import pprint
 import config
-cluster = MongoClient(config.MongoUri)
+
+cluster = MongoClient(config.MongoURI)#config.py содержит только монго токен
 db = cluster["test"] #DB name
 collection = db["test"] #Cluster name
 InputFlag = True
-choose = int(input("Введите 0 если хотите ввести содержимое файла NameList .txt\nВведите 1 если хотете добавить по одному\nВведите 2 для удаления\n"))
+
+choose = int(input("Введите 0 если хотите ввести содержимое файла NameList .txt\nВведите 1 если хотете добавить по одному\nВведите 2 для удаления\nВведите 3 для просмотра содержимого коллекции\n\n"))
 if choose == 0:
     i = int(input("С какой UserID начинать вставку\n"))
     t = open('NameList .txt', 'r', encoding="UTF-8")
@@ -12,18 +15,27 @@ if choose == 0:
         line = line[0:-1]
         collection.insert_one({'UserID': i, 'Username': line, 'Flag': 'false'})
         i += 1
-    print("Сделано\n")
+
 
 elif choose == 1:
-    while(InputFlag):
+    while InputFlag:
         i = int(input('В какую позицию?\n'))
         EnteredName = input("Введите имя\n")
         collection.delete_one({'UserID': i})
         collection.insert_one({'UserID': i, 'Username': EnteredName, 'Flag': 'false'})
         i += 1
         InputFlag = bool(input("Eще?\nДа - 1\nНет - 0\n"))
-    print("Сделано\n")
+
 
 elif choose == 2:
     collection.delete_many({})
-    print("Сделано\n")
+
+
+elif choose == 3:
+    count = 0
+    while count < collection.estimated_document_count():
+        pprint.pprint(db.test.find_one({}))
+        count = count + 1
+
+print("Сделано\n")
+
